a8eb5e0eea0f5f12ac09c0408e9a1cd0ab35c457
erowa\martin.buehlmann <martin.buehlmann@erowa.com>
2023-12-03 20:18:09 +0100

    Adapt default settings.

--- Source/RaspiRobot.RobotControl.GrabIt/Settings/DefaultCellSettingsProvider.cs
+++ Source/RaspiRobot.RobotControl.GrabIt/Settings/DefaultCellSettingsProvider.cs
@@ -6,7 +6,25 @@ using RaspiRobot.RobotControl.Settings;
 public class DefaultCellSettingsProvider : IDefaultCellSettingsProvider
 {
     public CellSettings DefaultCellSettings { get; } =
-        new CellSettings(new RobotSettings("GrabIt", new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 99)}}}}))
+        new CellSettings(new RobotSettings("GrabIt",
+            new Sequence
+            {
+                Steps =
+                {
+                    new Step
+                    {
+                        Positions =
+                        {
+                            new GrabItPosition(0, 112),
+                            new GrabItPosition(1, 112),
+                            new GrabItPosition(2, 112),
+                            new GrabItPosition(3, 112),
+                            new GrabItPosition(4, 112),
+                            new GrabItPosition(5, 112),
+                        },
+                    },
+                },
+            }))
         {
             Machines =
             {
@@ -16,8 +34,42 @@ public class DefaultCellSettingsProvider : IDefaultCellSettingsProvider
                     {
                         new ChuckSettings(
                             1,
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 100)}}}},
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 101)}}}}),
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 100),
+                                            new GrabItPosition(1, 100),
+                                            new GrabItPosition(2, 100),
+                                            new GrabItPosition(3, 100),
+                                            new GrabItPosition(4, 100),
+                                            new GrabItPosition(5, 100),
+                                        },
+                                    },
+                                },
+                            },
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 101),
+                                            new GrabItPosition(1, 101),
+                                            new GrabItPosition(2, 101),
+                                            new GrabItPosition(3, 101),
+                                            new GrabItPosition(4, 101),
+                                            new GrabItPosition(5, 101),
+                                        },
+                                    },
+                                },
+                            }),
                     },
                 },
             },
@@ -29,8 +81,42 @@ public class DefaultCellSettingsProvider : IDefaultCellSettingsProvider
                     {
                         new PlaceSettings(
                             1,
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 102)}}}},
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 103)}}}}),
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 102),
+                                            new GrabItPosition(1, 102),
+                                            new GrabItPosition(2, 102),
+                                            new GrabItPosition(3, 102),
+                                            new GrabItPosition(4, 102),
+                                            new GrabItPosition(5, 102),
+                                        },
+                                    },
+                                },
+                            },
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 103),
+                                            new GrabItPosition(1, 103),
+                                            new GrabItPosition(2, 103),
+                                            new GrabItPosition(3, 103),
+                                            new GrabItPosition(4, 103),
+                                            new GrabItPosition(5, 103),
+                                        },
+                                    },
+                                },
+                            }),
                     },
                 },
                 new MagazineSettings(2, "Mag")
@@ -39,20 +125,156 @@ public class DefaultCellSettingsProvider : IDefaultCellSettingsProvider
                     {
                         new PlaceSettings(
                             1,
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 104)}}}},
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 105)}}}}),
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 104),
+                                            new GrabItPosition(1, 104),
+                                            new GrabItPosition(2, 104),
+                                            new GrabItPosition(3, 104),
+                                            new GrabItPosition(4, 104),
+                                            new GrabItPosition(5, 104),
+                                        },
+                                    },
+                                },
+                            },
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 105),
+                                            new GrabItPosition(1, 105),
+                                            new GrabItPosition(2, 105),
+                                            new GrabItPosition(3, 105),
+                                            new GrabItPosition(4, 105),
+                                            new GrabItPosition(5, 105),
+                                        },
+                                    },
+                                },
+                            }),
                         new PlaceSettings(
                             2,
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 106)}}}},
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 107)}}}}),
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 106),
+                                            new GrabItPosition(1, 106),
+                                            new GrabItPosition(2, 106),
+                                            new GrabItPosition(3, 106),
+                                            new GrabItPosition(4, 106),
+                                            new GrabItPosition(5, 106),
+                                        },
+                                    },
+                                },
+                            },
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 107),
+                                            new GrabItPosition(1, 107),
+                                            new GrabItPosition(2, 107),
+                                            new GrabItPosition(3, 107),
+                                            new GrabItPosition(4, 107),
+                                            new GrabItPosition(5, 107),
+                                        },
+                                    },
+                                },
+                            }),
                         new PlaceSettings(
                             3,
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 106)}}}},
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 107)}}}}),
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 108),
+                                            new GrabItPosition(1, 108),
+                                            new GrabItPosition(2, 108),
+                                            new GrabItPosition(3, 108),
+                                            new GrabItPosition(4, 108),
+                                            new GrabItPosition(5, 108),
+                                        },
+                                    },
+                                },
+                            },
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 109),
+                                            new GrabItPosition(1, 109),
+                                            new GrabItPosition(2, 109),
+                                            new GrabItPosition(3, 109),
+                                            new GrabItPosition(4, 109),
+                                            new GrabItPosition(5, 109),
+                                        },
+                                    },
+                                },
+                            }),
                         new PlaceSettings(
                             4,
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 108)}}}},
-                            new Sequence {Steps = {new Step {Positions = {new GrabItPosition(0, 109)}}}}),
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 110),
+                                            new GrabItPosition(1, 110),
+                                            new GrabItPosition(2, 110),
+                                            new GrabItPosition(3, 110),
+                                            new GrabItPosition(4, 110),
+                                            new GrabItPosition(5, 110),
+                                        },
+                                    },
+                                },
+                            },
+                            new Sequence
+                            {
+                                Steps =
+                                {
+                                    new Step
+                                    {
+                                        Positions =
+                                        {
+                                            new GrabItPosition(0, 111),
+                                            new GrabItPosition(1, 111),
+                                            new GrabItPosition(2, 111),
+                                            new GrabItPosition(3, 111),
+                                            new GrabItPosition(4, 111),
+                                            new GrabItPosition(5, 111),
+                                        },
+                                    },
+                                },
+                            }),
                     },
                 },
             },
