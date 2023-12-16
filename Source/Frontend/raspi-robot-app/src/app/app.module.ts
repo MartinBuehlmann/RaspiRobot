1d0232499424df6bccbf6a91d4a3949054041304
Martin Bühlmann <martin.buehlmann@gmx.ch>
2023-12-16 17:11:49 +0100

    Initial Angular app started

---
+++ Source/Frontend/raspi-robot-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DevicesComponent } from './devices/devices.component';
+import { MdiComponent } from './mdi/mdi.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DevicesComponent,
+    MdiComponent,
+    AboutComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Source/Frontend/raspi-robot-app/src/app/mdi/mdi.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mdi',
+  templateUrl: './mdi.component.html',
+  styleUrl: './mdi.component.scss'
+})
+export class MdiComponent {
+
+}
