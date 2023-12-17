3f766075c8705ff67fa1bfb266a54a219ffdac1c
Martin Bühlmann <martin.buehlmann@gmx.ch>
2023-12-17 18:28:04 +0100

    Adapt windows deployment script.

--- build/DeployLocalWindows.bat
+++ build/DeployLocalWindows.bat
@@ -1,3 +1,15 @@
-CD ..\Source
-RD ..\..\artifacts /s /q
+@echo off
+cd ..\Source
+if exist ..\..\artifacts rd ..\..\artifacts /s /q
+
+echo Build .NET backend...
+cd Backend
 dotnet publish RaspiRobot -c Release -r win-x64 --self-contained -o ../../artifacts/publish/windows /p:DebugType=None /p:DebugSymbols=false
+cd ..
+echo .NET backend built.
+
+echo Copying frontend to backend for delivery...
+copy ..\artifacts\frontend\browser\*.* ..\artifacts\publish\windows\wwwroot
+echo Finished.
+
+pause
