610b88e48d12a3e084980b8b7942f13c3aa81dff
Martin Bühlmann <martin.buehlmann@erowa.com>
2023-12-19 05:39:49 +0100

    Separate interpolation from TransportSequenceExecutor.

--- Source/Backend/RaspiRobot.RobotControl.GrabIt/Devices/Robot/TransportSequence/TransportSequenceExecutor.cs
+++ Source/Backend/RaspiRobot.RobotControl.GrabIt/Devices/Robot/TransportSequence/TransportSequenceExecutor.cs
@@ -1,6 +1,5 @@
 ﻿namespace RaspiRobot.RobotControl.GrabIt.Devices.Robot.TransportSequence;
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -9,6 +8,13 @@ using RaspiRobot.RobotControl.Settings;
 
 internal class TransportSequenceExecutor
 {
+    private readonly TransportSequenceStepInterpolator interpolator;
+
+    public TransportSequenceExecutor(TransportSequenceStepInterpolator interpolator)
+    {
+        this.interpolator = interpolator;
+    }
+
     public void Execute(IReadOnlyList<Sequence> sequences, IGrabItDriver driver)
     {
         foreach (Sequence sequence in sequences)
@@ -22,7 +28,7 @@ internal class TransportSequenceExecutor
 
                 if (currentDrivePositions.Any())
                 {
-                    positions = this.Interpolate(
+                    positions = this.interpolator.Interpolate(
                         positions,
                         currentDrivePositions);
                 }
@@ -36,28 +42,4 @@ internal class TransportSequenceExecutor
         }
     }
 
-    private IReadOnlyList<GrabItPosition> Interpolate(
-        IReadOnlyList<GrabItPosition> positions,
-        IReadOnlyDictionary<byte, int> currentDrivePositions)
-    {
-        int stepCount = 1;
-        foreach (GrabItPosition position in positions)
-        {
-            stepCount = (int)(Math.Max(stepCount, Math.Abs(position.Value - currentDrivePositions[position.Drive])) * 0.8);
-        }
-
-        var interpolatedPositions = new List<GrabItPosition>();
-        for (int i = 1; i <= stepCount; i++)
-        {
-            foreach (GrabItPosition position in positions)
-            {
-                int interpolatedValue = currentDrivePositions[position.Drive] +
-                                        ((position.Value - currentDrivePositions[position.Drive]) * i / stepCount);
-                var interpolatedPosition = new GrabItPosition(position.Drive, interpolatedValue);
-                interpolatedPositions.Add(interpolatedPosition);
-            }
-        }
-
-        return interpolatedPositions;
-    }
 }
---
+++ Source/Backend/RaspiRobot.RobotControl.GrabIt/Devices/Robot/TransportSequence/TransportSequenceStepInterpolator.cs
@@ -0,0 +1,33 @@
+﻿namespace RaspiRobot.RobotControl.GrabIt.Devices.Robot.TransportSequence;
+
+using System;
+using System.Collections.Generic;
+using RaspiRobot.RobotControl.GrabIt.Settings;
+
+internal class TransportSequenceStepInterpolator
+{
+    public IReadOnlyList<GrabItPosition> Interpolate(
+        IReadOnlyList<GrabItPosition> positions,
+        IReadOnlyDictionary<byte, int> currentDrivePositions)
+    {
+        int stepCount = 1;
+        foreach (GrabItPosition position in positions)
+        {
+            stepCount = (int)(Math.Max(stepCount, Math.Abs(position.Value - currentDrivePositions[position.Drive])) * 0.8);
+        }
+
+        var interpolatedPositions = new List<GrabItPosition>();
+        for (int i = 1; i <= stepCount; i++)
+        {
+            foreach (GrabItPosition position in positions)
+            {
+                int interpolatedValue = currentDrivePositions[position.Drive] +
+                                        ((position.Value - currentDrivePositions[position.Drive]) * i / stepCount);
+                var interpolatedPosition = new GrabItPosition(position.Drive, interpolatedValue);
+                interpolatedPositions.Add(interpolatedPosition);
+            }
+        }
+
+        return interpolatedPositions;
+    }
+}
