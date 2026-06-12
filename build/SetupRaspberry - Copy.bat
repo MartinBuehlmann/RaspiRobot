5a5c4850cca778993988f9c272da991164b814b8
Martin Bühlmann <martin.buehlmann@erowa.com>
2026-06-12 12:43:39 +0200

    revert

--- build/SetupRaspberry - Copy.bat
+++
@@ -1,8 +0,0 @@
-SET address=raspirobot.%USERDNSDOMAIN%
-SET password=pocketpc2002
-SET product=RaspiRobot
-plink -ssh pi@%address% -pw %password% -no-antispoof "mkdir /home/pi/%product%"
-plink -ssh pi@%address% -pw %password% -no-antispoof "mkdir /home/pi/%product%/bin"
-scp -r %product%.service pi@%address%:/lib/systemd/system/
-plink -ssh pi@%address% -pw %password% -no-antispoof "sudo systemd daemon-reload"
-plink -ssh pi@%address% -pw %password% -no-antispoof "sudo systemctl enable %product%"
